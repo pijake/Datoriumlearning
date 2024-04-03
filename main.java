c5eb8ec9136c9f5b780a3cbe3be3454c5426052b
pijake <109518740+pijake@users.noreply.github.com>
2024-04-03 19:18:49 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,27 @@
+public class Main {
+  public static void main(String[] args) {
+
+    // winter, spring, summer, autumn
+    // warm jacket, t-shirt, swimming suite, rain coat
+
+      String season = "sunset";
+
+      if (season == "winter") {
+        System.out.println("Wear a warm jacket!");
+      }
+      else if (season == "spring") {
+        System.out.println("Wear a T-shirt!");
+      }
+      else if (season == "summer") {
+        System.out.println("Wear a swimming suite!");
+      }
+      else if (season == "autumn") {
+        System.out.println("Wear a rain coat!");
+      }
+    else {
+      System.out.println("Invalid season");
+    }
+    }
+
+    
+  }
